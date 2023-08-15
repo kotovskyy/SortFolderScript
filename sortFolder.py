f1135e18eeb60f82ea830592ba8878bef49a7e97
kotovskyy <alexkondratru@gmail.com>
2023-08-15 14:59:43 +0200

    Add comments in sortFolder.py

--- sortFolder.py
+++ sortFolder.py
@@ -2,17 +2,27 @@ import os
 import sys
 import shutil
 
+# first argument: path to dir to sort
 dirpath = sys.argv[1]
+# get all files from `dirpath`
 files = os.listdir(dirpath)
-regular_files = [file for file in files if os.path.isfile(dirpath + "/" + file)]
+# get all regular files from `files` list
+reg_files = [file for file in files if os.path.isfile(dirpath + "/" + file)]
+# get all directories from `files` list
 directories = [file for file in files if os.path.isdir(dirpath + "/" + file)]
-file_extensions = [file.split(sep='.', maxsplit=1)[1] for file in regular_files]
+# get all files' extensions
+file_extensions = [file.split(sep='.', maxsplit=1)[1] for file in reg_files]
+# create a set from extensions list
 file_extensions_set = set(file_extensions)
 
 for ext in file_extensions_set:
+    # if folder with extension name doesn't exist in `dirpath`
     if not os.path.exists(dirpath + "/" + ext):
+        # create folder with name `ext` in `dirpath`
         os.mkdir(path=dirpath+"/"+ext)
-    for file in regular_files:
+    for file in reg_files:
+        # for every regular file in `reg_files` list
+        # check extension. If ext matches -> move file in `dirpath/ext`
         if file.split(sep='.', maxsplit=1)[-1] != ext:
             continue
         source_filepath = dirpath+"/"+file
@@ -20,5 +30,6 @@ for ext in file_extensions_set:
         shutil.move(src=source_filepath,dst=destination_filepath)
 
 for dir in directories:
+    # check for empty directories in `dirpath` and remove them
     if os.listdir(path=dirpath+"/"+dir) == []:
         os.rmdir(dirpath+"/"+dir)
